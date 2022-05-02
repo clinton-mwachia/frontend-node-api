dbf86a3aa819a6b7361c245c3f6fcf6629148534
clinton moshe mwachia <clintonmwachia9@gmail.com>
2022-05-02 09:35:43 +0300

    added add todo route

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import { lazy } from "react";
 
 const profile = lazy(() => import("./views/Profile"));
-const todos = lazy(() => import("./views/Todos"));
+const todos = lazy(() => import("./views/todos/Todos"));
+const AddTodos = lazy(() => import("./views/todos/AddTodo"));
 
 const routes = [
   { path: "/", exact: true, name: "Home" },
   { path: "/todos", name: "Todos", component: todos },
+  { path: "/add/todo", name: "AddTodo", component: AddTodos },
   { path: "/profile", name: "Profile", component: profile },
 ];
 
