3c6b33c5dd1c4101e089764d1aab54f1f13fc09d
clinton moshe mwachia <clintonmwachia9@gmail.com>
2022-05-02 11:12:16 +0300

    added link to redirect back to the todos

--- src/views/todos/AddTodo.js
+++ src/views/todos/AddTodo.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const AddTodo = () => {
-  return <div>AddTodo</div>;
+  return (
+    <div>
+      AddTodo <Link to="/todos">Back to Todos</Link>
+    </div>
+  );
 };
 
 export default AddTodo;
