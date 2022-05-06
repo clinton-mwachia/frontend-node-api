ec70552a499fc9d3efe8ea4c5e05eb1cc96cb564
clinton moshe mwachia <clintonmwachia9@gmail.com>
2022-05-06 11:09:47 +0300

    mapped data into cards and implemented no data page

--- src/views/todos/Todos.js
+++ src/views/todos/Todos.js
@@ -13,6 +13,7 @@ import {
 import { Link } from "react-router-dom";
 
 import Axios from "../Axios";
+import NoDataPage from "../pages/NoDataPage";
 
 const Todos = () => {
   const [todos, setTodos] = React.useState([]);
@@ -21,6 +22,44 @@ const Todos = () => {
     Axios.get("/todo").then((res) => setTodos(res.data));
   }, []);
   console.log(todos);
+  const data = [
+    {
+      Title: "Card title",
+      Text: "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+      Footer: "Last updated 3 mins ago",
+    },
+    {
+      Title: "Card title",
+      Text: "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+      Footer: "Last updated 3 mins ago",
+    },
+    {
+      Title: "Card title",
+      Text: "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+      Footer: "Last updated 3 mins ago",
+    },
+    {
+      Title: "Card title",
+      Text: "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+      Footer: "Last updated 3 mins ago",
+    },
+    {
+      Title: "Card title",
+      Text: "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+      Footer: "Last updated 3 mins ago",
+    },
+    {
+      Title: "Card title",
+      Text: "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+      Footer: "Last updated 3 mins ago",
+    },
+    {
+      Title: "Card title",
+      Text: "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+      Footer: "Last updated 3 mins ago",
+    },
+  ];
+
   return (
     <div>
       <Link to="/add/todo">
@@ -29,73 +68,25 @@ const Todos = () => {
       <hr />
       <CContainer>
         <CRow xs={{ cols: 1 }} md={{ cols: 3 }} className="g-4">
-          <CCol xs>
-            <CCard className="h-100">
-              <CCardBody>
-                <CCardTitle>Card title</CCardTitle>
-                <CCardText>
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </CCardText>
-              </CCardBody>
-              <CCardFooter>
-                <small className="text-medium-emphasis">
-                  Last updated 3 mins ago
-                </small>
-              </CCardFooter>
-            </CCard>
-          </CCol>
-          <CCol xs>
-            <CCard className="h-100">
-              <CCardBody>
-                <CCardTitle>Card title</CCardTitle>
-                <CCardText>
-                  This card has supporting text below as a natural lead-in to
-                  additional content.
-                </CCardText>
-              </CCardBody>
-              <CCardFooter>
-                <small className="text-medium-emphasis">
-                  Last updated 3 mins ago
-                </small>
-              </CCardFooter>
-            </CCard>
-          </CCol>
-          <CCol xs>
-            <CCard className="h-100">
-              <CCardBody>
-                <CCardTitle>Card title</CCardTitle>
-                <CCardText>
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This card has even longer
-                  content than the first to show that equal height action.
-                </CCardText>
-              </CCardBody>
-              <CCardFooter>
-                <small className="text-medium-emphasis">
-                  Last updated 3 mins ago
-                </small>
-              </CCardFooter>
-            </CCard>
-          </CCol>
-          <CCol xs>
-            <CCard className="h-100">
-              <CCardBody>
-                <CCardTitle>Card title</CCardTitle>
-                <CCardText>
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </CCardText>
-              </CCardBody>
-              <CCardFooter>
-                <small className="text-medium-emphasis">
-                  Last updated 3 mins ago
-                </small>
-              </CCardFooter>
-            </CCard>
-          </CCol>
+          {data.length > 0 ? (
+            data.map((item) => (
+              <CCol xs>
+                <CCard className="h-100">
+                  <CCardBody>
+                    <CCardTitle>{item.Title}</CCardTitle>
+                    <CCardText>{item.Text}</CCardText>
+                  </CCardBody>
+                  <CCardFooter>
+                    <small className="text-medium-emphasis">
+                      {item.Footer}
+                    </small>
+                  </CCardFooter>
+                </CCard>
+              </CCol>
+            ))
+          ) : (
+            <NoDataPage />
+          )}
         </CRow>
       </CContainer>
     </div>
