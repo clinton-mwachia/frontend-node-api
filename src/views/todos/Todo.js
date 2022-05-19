2574cee7ed5496d572736d82288490f819d84a2d
clinton moshe mwachia <clintonmwachia9@gmail.com>
2022-05-19 13:32:31 +0300

    getting todo by id

--- src/views/todos/Todo.js
+++ src/views/todos/Todo.js
@@ -1,13 +1,25 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+
+import Axios from "../Axios";
 
 const Todo = () => {
+  const [todo, setTodo] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    Axios.get(`/todo/${id}`)
+      .then((res) => setTodo(res.data))
+      .catch((err) => alert(err));
+  }, [id]);
   return (
     <div>
-        <h4>Todo</h4>
-        <Link to="/todos">Back To Todos</Link>
+      <h4>Todo</h4>
+      <Link to="/todos">Back To Todos</Link>
+      <p>id: {todo._id}</p>
+      <hr />
     </div>
-  )
-}
+  );
+};
 
-export default Todo
+export default Todo;
