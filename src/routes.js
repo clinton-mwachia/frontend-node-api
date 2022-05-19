4d9a863376c1e6aa2d32fa6df6f7fd505e00824f
clinton moshe mwachia <clintonmwachia9@gmail.com>
2022-05-19 13:31:02 +0300

    added a route for getting todo by id

--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,14 @@ import { lazy } from "react";
 const profile = lazy(() => import("./views/Profile"));
 const todos = lazy(() => import("./views/todos/Todos"));
 const AddTodos = lazy(() => import("./views/todos/AddTodo"));
-const Todo = lazy(() => import("./views/todos/Todo"))
+const Todo = lazy(() => import("./views/todos/Todo"));
 
 const routes = [
   { path: "/", exact: true, name: "Home" },
   { path: "/todos", name: "Todos", component: todos },
   { path: "/add/todo", name: "AddTodo", component: AddTodos },
   { path: "/profile", name: "Profile", component: profile },
-  { path: "/todo/id", name: "Todo", component: Todo },
+  { path: "/todo/:id", name: "Todo", component: Todo },
 ];
 
 export default routes;
